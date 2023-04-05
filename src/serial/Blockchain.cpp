12fddd8425fd9e42bc97736bd9cde3f494a08b68
SomaSz <soma.szabo15@gmail.com>
2023-04-04 19:17:56 -0500

    Created Blockchain object

---
+++ src/serial/Blockchain.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Blockchain {
+   private:
+    // Block class. Used to store the data for each block in the blockchain.
+    class Block {
+       public:
+        size_t block_id;
+        string prev_digest;
+        string data;
+        size_t threshold;
+        size_t nonce;
+        Block *next;
+    };
+    Block *head;
+    Block *current;
+    size_t num_blocks;
+    size_t block_counter;
+
+   public:
+    Blockchain();
+    ~Blockchain();
+    bool isEmpty() { return (head == NULL); }
+    void appendBlock(string prev_digest, string data, size_t threshold, size_t nonce);
+    void removeBlock();
+    bool thresholdMet(string &digest, size_t threshold);
+    string getString();
+    size_t getCurrentBlockId() { return current->block_id; }
+    size_t getSize() { return num_blocks; }
+};
+
+/**
+ * @brief Construct a new Blockchain object
+ *
+ */
+Blockchain::Blockchain() {
+    head = NULL;
+    current = NULL;
+    num_blocks = 0;
+    block_counter = 0;
+}
+
+/**
+ * @brief Destroy the Blockchain object
+ *
+ */
+Blockchain::~Blockchain() {
+    while (!isEmpty()) {
+        removeBlock();
+    }
+}
+
+/**
+ * @brief Appends a block to the end of the blockchain. Beyond the current pointer.
+ *
+ * @param prev_digest
+ * @param data
+ * @param threshold
+ * @param nonce
+ */
+void Blockchain::appendBlock(string prev_digest, string data, size_t threshold, size_t nonce) {
+    Block *new_block = new Block();
+    new_block->block_id = block_counter;
+    new_block->nonce = nonce;
+    new_block->prev_digest = prev_digest;
+    new_block->data = data;
+    new_block->threshold = threshold;
+    new_block->next = NULL;
+
+    if (isEmpty()) {
+        head = new_block;
+        current = new_block;
+    } else {
+        current->next = new_block;
+        current = new_block;
+    }
+    num_blocks++;
+    block_counter++;
+}
+
+/**
+ * @brief Removes a block from the front of the blockchain.
+ *
+ */
+void Blockchain::removeBlock() {
+    if (!isEmpty()) {
+        Block *temp = head;
+        head = head->next;
+        delete temp;
+        num_blocks--;
+    }
+}
+
+/**
+ * @brief Checks if the digest has exactly threshold number of leading zeros.
+ *
+ * @param digest
+ * @param threshold
+ * @return true
+ * @return false
+ */
+bool Blockchain::thresholdMet(string &digest, size_t threshold) {
+    for (size_t i = 0; i < threshold; i++) {
+        if (digest[i] != '0') {
+            return false;
+        }
+    }
+
+    // If the current digest has more than the threshold number of leading zeros, then it is not a valid digest.
+    if (digest[current->block_id] == '0') {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * @brief Returns the string representation of the current block.
+ *
+ * @return string
+ */
+string Blockchain::getString() {
+    return "[" + to_string(current->block_id) + "|" + current->prev_digest + "|" + current->data + "|" + to_string(current->threshold) + "|" + to_string(current->nonce) + "]";
+}
--- src/serial/btc_miner_serial.cpp
+++ src/serial/btc_miner_serial.cpp
@@ -1,12 +1,25 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <iostream>
+#include <stdlib.h>
+
+#include "Blockchain.cpp"
 
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+    const string init_prev_digest = "0000000000000000000000000000000000000000000000000000000000000000";
+    const string init_data = "This is the initial data in the 1st block";
 
-  return 0;
+    size_t global_threshold = 1;
+    size_t global_nonce = 0;
+
+    Blockchain blockchain;
+    blockchain.appendBlock(init_prev_digest, init_data, global_threshold, global_nonce);
+
+    cout << "Appended Block with ID: " << blockchain.getCurrentBlockId() << endl;
+    cout << "Blockchain size: " << blockchain.getSize() << endl;
+
+    string chain_str = blockchain.getString();
+    cout << "Blockchain string: " << chain_str << endl;
+
+    return 0;
 }

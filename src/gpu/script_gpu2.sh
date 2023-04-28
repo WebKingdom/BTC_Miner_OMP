7d8cfad7a9cd08a6f07d837e6ab4a4a7276c1ab4
Soma Szabo - student <sszabo@nova.its.iastate.edu>
2023-04-28 02:16:20 -0500

    HPC trials

--- src/gpu/script_gpu2.sh
+++ src/gpu/script_gpu2.sh
@@ -4,17 +4,20 @@
 #    sbatch thefilename
 # job standard output will go to the file slurm-%j.out (where %j is the job ID)
 
-#SBATCH --time=00:15:00                 # walltime limit (HH:MM:SS)
-#SBATCH --nodes=1                       # number of nodes
-#SBATCH --ntasks-per-node=64            # 64 processor core(s) per node 
-#SBATCH --mem=32G                       # maximum memory per node
-#SBATCH --partition=class-short         # class node(s)
-#SBATCH --job-name="ssz_parallel2"
-#SBATCH --output="hpc_parallel2-%j.out" # job standard output file (%j replaced by job id)
+#SBATCH --time=00:15:00               # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1                     # number of nodes
+#SBATCH --ntasks-per-node=24          # 24 processor core(s) per node 
+#SBATCH --mem=64G                     # maximum memory per node
+#SBATCH --gres=gpu:a100:4
+#SBATCH --partition=class-gpu-short   # class node(s)
+#SBATCH --job-name="ssz_gpu1"
+#SBATCH --output="hpc_gpu1-%j.out"    # job standard output file (%j replaced by job id)
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
 # module load intel
+# module load nvhpc
+module load cuda
 module load gcc
 module load openssl
 module load openmpi_hpc
@@ -28,7 +31,7 @@ make clean
 make
 
 # run executable in background and get the PID of the process
-./btc_miner_parallel.exe &
+./btc_miner_gpu.exe &
 MINER_PID=$!
 sleep $TIMEOUT
 kill -2 $MINER_PID
